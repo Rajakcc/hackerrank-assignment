28575e94bc93c62a18ace095c7d540b5862ca04d
Rajakcc <rajarams203btechdscse2024@kccitm.edu.in>
2023-02-16 14:14:31 +0530

    first commit

---
+++ Assignment Hackerank Grading students.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    rounded_grades=[]
+    for i in grades:
+        if i>=38:
+            difference=5-i%5
+            if difference<3:
+                i=i+difference
+        rounded_grades.append(i)
+    return rounded_grades           
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
