7dc80a4426e71dafea7dc620fab98d46fa0abe65
Rajakcc <rajarams203btechdscse2024@kccitm.edu.in>
2023-03-01 13:36:23 +0530

    breaking

---
+++ assignment 1 breaking recirds.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'breakingRecords' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY scores as parameter.
+#
+
+def breakingRecords(scores):
+    high = low = scores[0]
+    h_score = l_score = 0
+    for i in scores:
+        if i > high:
+            h_score += 1
+            high = i
+        if i < low:
+            l_score += 1
+            low = i
+    return((h_score, l_score))
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
